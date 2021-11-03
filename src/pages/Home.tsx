3e2c11ed3d8de93b6d64eab8617aa4c79a8e1d4d
Ngo Tien Dung <67892236+tiendung-2000@users.noreply.github.com>
2021-11-03 17:01:01 +0700

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Home</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
